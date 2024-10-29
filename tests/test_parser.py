2c17b0af8419293a27fdbe07743eb1bcbc3fb4b4
Lele Gaifax <lele@metapensiero.it>
2024-10-29 07:47:59 +0100

    Add expected-to-fail test against issue #157

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -244,3 +244,32 @@ def test_parse_sql_json():
     '''
 
     assert used_tables(turkish_chars) == {'Schema.Table', 'Schema2.Table2'}
+
+
+@pytest.mark.xfail
+def test_issue157():
+    # See https://github.com/pganalyze/libpg_query/issues/261
+
+    query = """\
+CREATE OR REPLACE FUNCTION update_customer_total_sales(customer_id INT)
+RETURNS VOID AS $$
+DECLARE
+    total NUMERIC;
+BEGIN
+    -- Calculate the total sales for the specified customer
+    SELECT COALESCE(SUM(amount), 0) INTO total
+    FROM orders
+    WHERE customer_id $1;   -- <------ this part is invalid
+
+    -- Update the total_sales in the customers table
+    UPDATE customers
+    SET total_sales = total
+    WHERE customer_id;
+
+    -- Optionally, you can return the total for confirmation
+    RAISE NOTICE 'Total sales for customer ID %: %', customer_id, total;
+END;
+$$ LANGUAGE plpgsql"""
+
+    with pytest.raises(Error) as exc:
+        parse_plpgsql(query)
